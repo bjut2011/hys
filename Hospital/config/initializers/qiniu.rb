e7b0b752c34a4381337b24ac8a79892fce2266e8
bjut2011 <chenfw0923@163.com>
2015-11-07 20:36:46 +0800

    1.3

---
+++ Hospital/config/initializers/qiniu.rb
@@ -0,0 +1,3 @@
+require 'qiniu'
+::Qiniu.establish_connection! :access_key => "23UPGX38WBcWdjtaJhBgQA6Vyg4tHAzM9_beDAk0",
+                              :secret_key => "DSxn_q4MErTsZ7P5B8tbEWywDkoNbXDeNU6C1gFO"
